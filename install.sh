0506f246ff66a0f9e31a8d85e40d49533bb3ef44
aido93 <aidos.tanatos@gmail.com>
2016-07-22 15:04:25 +0300

    fixed bugs=)

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 sudo apt-get install vim vim-snippets vim-tlib vim-youcompleteme cmake 
+vim-addon-manager install tlib snippets youcompleteme
 sudo mkdir /etc/cpprj
 sudo cp lics/* /etc/cpprj/
 sudo cp {CMakeLists.txt,Doxyfile,cli.cpp,lib.cpp} /etc/cpprj/
+sudo chmod -R 644 /etc/cpprj/*
 sudo cp cpprj /usr/bin
 mkdir ~/projects
 mkdir ~/.vim
