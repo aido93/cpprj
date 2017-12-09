a08407b9c4a2151dcdaf16de4b27d2b89f11da16
aido93 <aidos.tanatos@gmail.com>
2017-12-10 02:27:15 +0300

    Added setters, getters and autodetect

--- 1.0/class.py
+++ 1.0/class.py
@@ -1,4 +1,29 @@
 import date
+# move to cpp for finding private or protected vars
+from difflib import SequenceMatcher
+
+std={
+	'containers' 			: [ "vector","list","map","queue","deque","string","array","set","stack","forward_list","unordered_set","unordered_map"],
+	'memory'				: [ "auto_ptr","shared_ptr","weak_ptr","unique_ptr","auto_ptr_ref","default_delete",
+								"allocator","allocator_arg","allocator_arg_t","allocator_traits",
+								"enable_shared_from_this","owner_less","raw_storage_iterator","pointer_traits","pointer_safety"],
+	'chrono'				: [ "duration","time_point","system_clock","steady_clock","high_resolution_clock","treat_as_floating_point",
+								"duration_values","hours","minutes","seconds","milliseconds"],
+	'functional'			: [ "function",],
+	'initializer_list'		: [ "initializer_list",],
+	'random'				: [ "random_device",],
+	'string'				: [ "string","wstring","basic_string","u16string","u32string"],
+	'valarray'				: [ "valarray","slice","gslice","slice_array","gslice_array","mask_array","indirect_array"],
+	'utility'				: [ "pair",],
+	'mutex'					: [ "mutex","recursive_mutex","timed_mutex","recursive_timed_mutex","unique_lock","lock_guard","once_flag"],
+	'thread'				: [ "thread",],
+	'future'				: [ "future","promise","packaged_task","shared_future","future_status","future_error","future_errc"],
+	'condition_variable'	: [ "condition_variable","condition_variable_any","cv_status"],
+	'atomic'				: [ "atomic","atomic_flag"],
+	'tuple'					: [ "tuple","tuple_size","tuple_element"],
+	'complex'				: [ "complex",]
+}
+
 def header(class_name, author, email):
 	return """/**
  * Description: Interface to the """+class_name+"""-class
@@ -27,7 +52,7 @@ def namespace(namespace_name, body, tabstop):
  */
 namespace """+namespace_name+"""
 {
-"""+ts+"\n};\n"
+"""+ts+body.replace('\n',ts)+"\n}; //"+namespace.upper()+" namespace\n"
 
 def add_methods(methods, ts):
 	ret=""
@@ -60,9 +85,36 @@ def add_methods(methods, ts):
 	return ret
 
 def create_class(class_name,template_types=None, class_parents=None,
-				 private_vars=None,protected_vars=None,
+				 private_vars=None,protected_vars=None, 
+				 private_setters=False, private_getters=False, protected_setters=False, protected_getters=False,
 				 public_methods=None,protected_methods=None,private_methods=None,
 				 tabstop=4):
+	autodetected=[]
+	for v in private_vars:
+		for inc, t in stl:
+			if any("std::"+substring in v['type'] for substring in t):
+				if inc not in autodetected:
+					autodetected.append(inc)
+		else:
+			if v['type'][0]=='Q':
+				autodetected.append(v['type'])
+	for v in protected_vars:
+		for inc, t in stl:
+			if any("std::"+substring in v['type'] for substring in t):
+				if inc not in autodetected:
+					autodetected.append(inc)
+		else:
+			if v['type'][0]=='Q':
+				autodetected.append(v['type'])
+	for func in public_methods:
+		for v in func["vars"]:
+			for inc, t in stl:
+				if any("std::"+substring in v['type'] for substring in t):
+					if inc not in autodetected:
+						autodetected.append(inc)
+			else:
+				if v['type'][0]=='Q':
+					autodetected.append(v['type'])
 	ts=' '*tabstop
 	ret="""/**
  * \\brief
@@ -87,6 +139,38 @@ def create_class(class_name,template_types=None, class_parents=None,
 	ret=ret+"\n{\n"+ts+'public:\n';
 	if public_methods:
 		ret=ret+add_methods(public_methods, ts)
+	sgetters=[]
+	if protected_vars:
+		for v in protected_vars:
+			if protected_getters:
+				sgetters.append({ 	"type" : v['type'],
+									"name" : "get_"+v['name'],
+									"vars" : None,
+									"modifier": "const"})
+			if protected_setters:
+				sgetters.append({ 	"type" : 'void',
+									"name" : "set_"+v['name'],
+									"vars" : {
+												'type': "const "+v['type']+'&',
+												'name': '_'+var['name']
+											 },
+									"modifier": None})
+	if private_vars:
+		for v in private_vars:
+			if private_getters:
+				sgetters.append({ 	"type" : v['type'],
+									"name" : "get_"+v['name'],
+									"vars" : None,
+									"modifier": "const"})
+			if private_setters:
+				sgetters.append({ 	"type" : 'void',
+									"name" : "set_"+v['name'],
+									"vars" : {
+												'type': "const "+v['type']+'&',
+												'name': '_'+var['name']
+											 },
+									"modifier": None})
+	ret=ret+add_methods(sgetters, ts)
 	ret=ret+'\n'+ts*2
 	if protected_vars or protected_methods:
 		ret=ret+'\n'+ts+'protected:'
@@ -104,12 +188,12 @@ def create_class(class_name,template_types=None, class_parents=None,
 		ret=ret+add_methods(private_methods, ts)
 		ret=ret+'\n'+ts*2
 	ret=ret+"\n};"
-	return ret
+	return (autodetected, ret)
 
 # vd=0 - not virtual
 # vd=1 - virtual
 # vd=2 - pure virtual
-def constructors(class_name, dd=0, dc=0, dm=0, vd=0, custom=None):
+def basic_class_content(class_name, dd=0, dc=0, dm=0, vd=0, custom=None):
 	ret=[]
 	dummy={ "type" : None,
 			"name" : class_name,
@@ -176,6 +260,8 @@ def constructors(class_name, dd=0, dc=0, dm=0, vd=0, custom=None):
 		d1=dummy
 		d1["name"]="~"+class_name
 		d1["type"]="virtual"
-		d1["modifier"]=" =0"
+		d1["modifier"]="=0"
 		ret.append(d1)
 	return ret
+
+
